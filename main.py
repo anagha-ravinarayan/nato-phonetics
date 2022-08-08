4c1e13f3690587a1ea76e27b355276053fbce84d
Anagha Ravinarayan <aravinarayan@salesforce.com>
2022-08-08 18:07:33 +0530

    Exception Handling

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas
 
+
+def phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetics = [f"{letter} for {nato_dict[letter]}" for letter in word]
+    except KeyError as e:
+        print("Sorry, please only enter letters in the Alphabet.\n")
+        phonetic()
+    else:
+        print(f"The NATO Phonetics for your word are: \n{phonetics}")
+
+
 nato_file = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_file.iterrows()}
 
-word = input("Enter a word: ").upper()
-phonetics = [f"{letter} for {nato_dict[letter]}" for letter in word]
-
-print(f"The NATO Phonetics for your word are: \n{phonetics}")
+phonetic()
